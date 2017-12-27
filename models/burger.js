bd9a9186724ec451e86c54c7218be59086c827d8
Ferenc Gutierrez <ferenc.g87@gmail.com>
2017-12-27 12:17:10 -0600

    Initial Commit"

---
+++ models/burger.js
@@ -0,0 +1,19 @@
+var orm = require('../config/orm');
+
+module.exports = {
+    all: function(callback){
+        orm.selectAll('burgers', function(res){
+            callback(res);
+        });
+    },
+    create: function(cols, vals, callback){
+        orm.insertOne('burgers', cols, vals, function(res){
+            callback(res);
+        });
+    },
+    update: function(objColsVals, condition, callback){
+        orm.updateOne('burgers', objColsVals, condition, function(res){
+            callback(res);
+        });
+    }
+};
